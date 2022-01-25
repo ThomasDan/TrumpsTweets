3a7b4c3a0129b5589880f609487d955f433ab3b1
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-01-25 13:01:00 +0100

    Big Brogress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TrumpsTweets';
+
+  images = ["trump_1.jpg", "trump_2.png", "trump_3.jpg", "trump_4.jpg", "trump_5.jpg", "trump_6.jpg"];
+  currentBackground = '../assets/images/trump_1.jpg';
+
+  ngOnInit(): void {
+    this.backgroundCycle();
+  }
+
+  backgroundCycle(){
+    console.log('Background cycler started!');
+    (async () => {
+      for(let i = 0; i < this.images.length; i++){
+        console.log('Background cycler Looping! - ' + i);
+        await this.delay(1000);
+        this.currentBackground = '../assets/images/' + this.images[i];
+        if(i+1 == this.images.length){
+          i = 0;
+          console.log('Background cycler restarted!');
+        }
+      }
+    })
+  }
+
+  delay(ms: number){
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+import { TweetComponent } from './feed/tweet/tweet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    TweetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
